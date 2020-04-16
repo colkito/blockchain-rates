2e11ae00a7c8acd265cc6cfc8d6a5760595bbeda
COLCO <mario@hashdog.com>
2020-04-16 14:19:41 -0300

    improve tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,37 +2,70 @@ import nock from 'nock';
 
 import { get } from '../src';
 
-// const code = 'BRL';
-const symbol = 'R$';
-
 describe('Get all rates', () => {
   beforeEach(() => {
     nock('https://blockchain.info')
       .persist()
       .get('/ticker?cors=true')
       .reply(200, {
-        data: {
-          BRL: {
-            symbol
-          }
+        BRL: {
+          symbol: 'R$'
+        },
+        USD: {
+          symbol: '$'
         }
       });
   });
 
+  test('using promises', async () => {
+    const rates: any = await get();
+    expect(typeof rates).toEqual('object');
+    expect(typeof rates.BRL).toEqual('object');
+    expect(typeof rates.BRL.symbol).toEqual('string');
+    expect(typeof rates.USD).toEqual('object');
+    expect(typeof rates.USD.symbol).toEqual('string');
+  });
+
   test('using callback', () => {
-    get((err: any, rate) => {
-      expect(typeof rate).toEqual('object');
-      expect(typeof rate.data).toEqual('object');
-      expect(typeof rate.data.BRL).toEqual('object');
-      expect(typeof rate.data.BRL.symbol).toEqual('string');
+    get((err: any, rates) => {
+      expect(typeof rates).toEqual('object');
+      expect(typeof rates.BRL).toEqual('object');
+      expect(typeof rates.BRL.symbol).toEqual('string');
+      expect(typeof rates.USD).toEqual('object');
+      expect(typeof rates.USD.symbol).toEqual('string');
     });
   });
 
+});
+
+describe('Get a rate by code', () => {
+  const code = 'BRL';
+  const symbol = 'R$';
+
+  beforeEach(() => {
+    nock('https://blockchain.info')
+      .persist()
+      .get('/ticker?cors=true')
+      .reply(200, {
+        BRL: {
+          symbol: 'R$'
+        },
+        USD: {
+          symbol: '$'
+        }
+      });
+  });
+
   test('using promises', async () => {
-    const rate: any = await get();
-    expect(typeof rate).toEqual('object');
-    expect(typeof rate.data).toEqual('object');
-    expect(typeof rate.data.BRL).toEqual('object');
-    expect(typeof rate.data.BRL.symbol).toEqual('string');
+    const rates: any = await get(code);
+    expect(typeof rates).toEqual('object');
+    expect(rates.symbol).toEqual(symbol);
+  });
+
+  test('using callback', () => {
+    get(code, (err: any, rate) => {
+      expect(typeof rate).toEqual('object');
+      expect(rate.symbol).toEqual(symbol);
+    });
   });
 });
