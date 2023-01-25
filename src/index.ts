00a21ccecb032c011079836ba8859c27f30b65be
colkito <github@colkito.com>
2023-01-25 13:25:38 -0300

    update base code

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import * as https from 'https';
+import https, { RequestOptions } from 'https';
 
-export type Callback = (...args: any[]) => void;
-
-export type RateType = {
+export type RateObj = {
   '15m': number;
   last: number;
   buy: number;
@@ -10,58 +8,67 @@ export type RateType = {
   symbol: string;
 };
 
+export type RateResponse = RateObj | [RateObj];
+
+export type Callback = (error: Error | null, data?: RateResponse) => void;
+
 const DEFAULT_CODE = 'all';
 
-const returnPromise = (code: string, options: any): Promise<RateType> => {
+const defaultOptions: RequestOptions = {
+  host: 'blockchain.info',
+  path: '/ticker?cors=true',
+  headers: {},
+  agent: false,
+};
+
+const returnPromise = (code: string, options: RequestOptions): Promise<RateResponse> => {
   return new Promise((resolve, reject) => {
     returnCallback(code, options, (err, data) => {
       if (err) return reject(err);
-      return resolve(data);
+      return resolve(data as RateResponse);
     });
   });
 };
 
-const returnCallback = (code: string, options: any, callback: Callback): void => {
+const returnCallback = (code: string, options: RequestOptions, callback: Callback): void => {
   https
-    .get(options, (res: any) => {
+    .get(options, (res) => {
       let dataBuffer = '';
-      res.on('data', (chunk: any) => {
+
+      res.on('data', (chunk: Buffer) => {
         dataBuffer += chunk.toString('utf8');
       });
+
       res.on('end', () => {
         try {
           let data = JSON.parse(dataBuffer);
           if (code !== DEFAULT_CODE) {
             data = data[code.toUpperCase()];
-            if (!data) return new Error(`code "${code}" not found.`);
+            if (!data) throw new Error(`code "${code}" not found.`);
           }
+
           return callback(null, data);
         } catch (err) {
-          return callback(err);
+          return callback(err as Error);
         }
       });
     })
-    .on('error', (err: any) => {
-      return callback(err);
+    .on('error', (err) => {
+      return callback(err as Error);
     });
 };
 
 export const get = (
   code?: string | Callback,
   callback?: Callback,
-): Promise<RateType> | Promise<[RateType]> | void => {
-  const options = {
-    host: 'blockchain.info',
-    path: '/ticker?cors=true',
-    headers: {},
-    agent: false,
-  };
+): Promise<RateResponse> | void => {
+  const options = { ...defaultOptions };
   if (typeof code === 'function') {
     return returnCallback(DEFAULT_CODE, options, code);
   } else if (typeof code === 'string') {
     if (callback) return returnCallback(code, options, callback);
     return returnPromise(code, options);
-  } else if (typeof code === 'undefined') {
+  } else {
     return returnPromise(DEFAULT_CODE, options);
   }
 };
