d02f63aa3577aa5f570ba95226cfb5253eea2beb
colkito <github@colkito.com>
2023-01-25 13:25:54 -0300

    update example

--- example/rates-example.js
+++ example/rates-example.js
@@ -1,25 +1,25 @@
-const blockchainRates = require("../dist");
+const blockchainRates = require('../dist');
 
-const code = "BRL";
+const code = 'BRL';
 
 // Using Promises
-const ratesPromise = blockchainRates.get();
-ratesPromise
-  .then(rates => console.log("[Promise][All] Rates:", rates))
-  .catch(err => console.log("[Promise][All] Error:", err));
+blockchainRates
+  .get()
+  .then((rates) => console.log('[Promise][All] Rates:', rates))
+  .catch((err) => console.error('[Promise][All] Error:', err));
 
-const ratePromise = blockchainRates.get(code);
-ratePromise
-  .then(rate => console.log("[Promise][Code] Rate:", rate))
-  .catch(err => console.log("[Promise][Code] Error:", err));
+blockchainRates
+  .get(code)
+  .then((rate) => console.log(`[Promise][${code}] Rate:`, rate))
+  .catch((err) => console.error(`[Promise][${code}] Error:`, err));
 
 // Using Callbacks
 blockchainRates.get((err, res) => {
-  console.log("[Callback][All] Error:", err);
-  console.log("[Callback][All] Rates:", res);
+  console.error('[Callback][All] Error:', err);
+  console.log('[Callback][All] Rates:', res);
 });
 
 blockchainRates.get(code, (err, res) => {
-  console.log("[Callback][Code] Error:", err);
-  console.log("[Callback][Code] Rate:", res);
+  console.error(`[Callback][${code}] Error:`, err);
+  console.log(`[Callback][${code}] Rate:`, res);
 });
